baca29456143b26d75eaf0f8aced4a1fa591f37b
Christian Barkowsky <christian@plenta.io>
2026-03-13 11:21:29 +0100

    Hello Contao 5

--- src/Plenta/ContaoFriendlyCaptchaBundle/Classes/Contao/Forms/FormFriendlyCaptcha.php
+++ src/Plenta/ContaoFriendlyCaptchaBundle/Classes/Contao/Forms/FormFriendlyCaptcha.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /**
  * Plenta Friendly Captcha Bundle for Contao Open Source CMS
  *
- * @copyright     Copyright (c) 2023, Plenta.io
+ * @copyright     Copyright (c) 2023-2026, Plenta.io
  * @author        Plenta.io <https://plenta.io>
  * @link          https://github.com/plenta/
  */
